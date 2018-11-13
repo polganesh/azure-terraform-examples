93c3cebb4bc0086be995c59d1871d638dca82ec1
Ganesh Pol <polganesh@gmail.com>
2018-11-13 04:21:02 +0000

    resource group

---
+++ resource-group-example/resource-group-example.tf
@@ -0,0 +1,13 @@
+module resource_manager{
+	source="git::https://github.com/polganesh/azure-terraform-modules.git?ref=0.1.2v//resource-group"
+	location_id="euNorth"
+	location="northeurope"
+	environment="dev"
+	cost_centre="it"
+	project="dfs"
+	version_id="001"
+	build_date="20181112"
+	maintenance_day="sun"
+	maintenance_time="0030"
+	resource_group_seq_id="001"
+}
