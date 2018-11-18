fff555493d0003617876deb44205c6f182716976
Ganesh Pol <polganesh@gmail.com>
2018-11-18 15:33:28 +0000

    vnet multiple cidr block example

--- vnet-examples/vnet-example.tf
+++ vnet-examples/vnet-example.tf
@@ -1,5 +1,5 @@
 module vnet{
-	source="git::https://github.com/polganesh/azure-terraform-modules.git?ref=0.2.1v//virtual-network"
+	source="git::https://github.com/polganesh/azure-terraform-modules.git//virtual-network"
 	location_id="euNorth"
 	location="northeurope"
 	environment="dev"
@@ -11,7 +11,7 @@ module vnet{
 	maintenance_time= "0030"
 	vnet_seq_id="001"
 	seq_id= "001"
-	vnet_cidr= "10.240.0.0/16"
+	vnet_cidr_list= ["10.240.0.0/16","10.241.0.0/16"]
 	public_subnet_cidr_list=["10.240.1.0/24","10.240.2.0/24","10.240.3.0/24"]
 	private_app_subnet_cidr_list=["10.240.4.0/24","10.240.5.0/24","10.240.6.0/24"]
 	private_db_subnet_cidr_list=["10.240.7.0/24","10.240.8.0/24","10.240.9.0/24"]	
